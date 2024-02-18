01e9c2c8b4e7375120a18b9b77b07ccda0433ff3
Murat Muldashev <97629199+muldashev23@users.noreply.github.com>
2024-02-18 21:26:45 +0100

    Backend Controllers

---
+++ API/Servises/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Servises;
+
+public class MapperConfig: Profile
+{
+    public MapperConfig()
+    {
+        CreateMap<Items, ItemDTO>();
+       
+    }
+}
